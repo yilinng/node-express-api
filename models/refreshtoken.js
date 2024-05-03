cb963481e9adb3bf72c322e754090d13c158e887
Yiling Wei <31823305+yilinng@users.noreply.github.com>
2024-05-03 16:12:19 +0800

    Update refreshtoken.js
    remove expire_at

--- models/refreshtoken.js
+++ models/refreshtoken.js
@@ -9,16 +9,8 @@ const refreshtokenSchema = new mongoose.Schema({
     type: String,
     require: true,
   },
+      //https://stackoverflow.com/questions/38472125/delete-mongodb-document-at-specific-time
 
-  /*
-  expires_at: {
-    type: Date,
-    default: Date.now(),
-    //https://stackoverflow.com/questions/38472125/delete-mongodb-document-at-specific-time
-    expires: 1200,
-    //expired in 20 min
-  },
-  */
 })
 
 module.exports = mongoose.model('RefreshToken', refreshtokenSchema)
